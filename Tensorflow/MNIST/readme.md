13ad3dd226c79a8220b8694b26f9789c164ace40
MeteorickLiu <jianxiaoliu@yeah.net>
2020-03-31 11:42:39 +0800

    LenNet5
    Numpy CNN for template

--- Tensorflow/MNIST/readme.md
+++ Tensorflow/MNIST/readme.md
@@ -1,2 +1,3 @@
 [TensorFlow运作方式入门](http://www.tensorfly.cn/tfdoc/tutorials/mnist_tf.html)
 python .\fully_connected_feed.py --input_data_dir ./mnist --log_dir ./log
+#--input_data_dir, --log_dir有默认参数，如果不自定义，程序会报错
---
+++ project/LenNet5/gParam.py
@@ -0,0 +1,11 @@
+
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+TOP_PATH = './MNIST/mnist-png/'
+LAB_PATH = './MNIST/mnist-png/label.txt'
+C_SIZE = 5
+F_NUM = 12
+P_SIZE = 2
+FILE_TYPE = '.png'
+MAX_ITER_NUM = 22 #50
